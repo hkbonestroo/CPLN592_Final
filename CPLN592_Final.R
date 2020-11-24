5a128e441cf237a7df1bde92afa35657703533a8
hkb1414 <43580459+hkbones@users.noreply.github.com>
2020-11-24 08:55:50 -0500

    more of bike stuff

--- CPLN592_Final.R
+++ CPLN592_Final.R
@@ -99,7 +99,8 @@ q5 <- function(variable) {as.factor(ntile(variable, 5))}
 
 palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
 palette6 <- c("#C48C04", "#FA7800","#8FA108",   "#5AB60C", "#25CB10")
-
+palette4 <- c("#D2FBD4","#92BCAB","#527D82","#123F5A")
+palette2 <- c("#6baed6","#08519c")
 # Load nn function
 
 nn_function <- function(measureFrom,measureTo,k) { 
@@ -159,12 +160,19 @@ sfTracts <-
 `%nin%` = Negate(`%in%`)
 sfTracts <- subset(sfTracts, GEOID %nin% c("06075980401","06075017902","06075017902","06075017902"))
 
+#load neighborhoods
+neighborhoods <- 
+  st_read("https://data.sfgov.org/api/geospatial/pty2-tcw4?method=export&format=GeoJSON") %>%
+  st_transform("EPSG:6339") %>%
+  dplyr::select(-link)
+
+
 #load parking data
 
-ParkingMeters.dat <- st_read("Mayparking.csv")
-ParkingMeters <- subset(ParkingMeters.dat, Lot %in% "Street")
+ParkingMeters.dat <- st_read("SFMTA_Parking_Meter_Detailed_Revenue_Transactions.csv")
+ParkingMeters <- subset(ParkingMeters.dat, Lot %in% "street")
 ParkingMeters<- ParkingMeters%>%
-  dplyr::select(POST_ID,STREET_BLOCK, Address2, SESSION_START_DT, SESSION_END_DT, GROSS_PAID_AMT)
+  dplyr::select(POST_ID,STREET_BLOCK, SESSION_START_DT, SESSION_END_DT, GROSS_PAID_AMT)
 ParkingMeters <- ParkingMeters %>%
   mutate(interval60 = floor_date(mdy_hm(SESSION_END_DT), unit = "hour"),
          interval15 = floor_date(mdy_hm(SESSION_END_DT), unit = "15 mins"),
@@ -212,7 +220,7 @@ ParkingMeters.sf<- st_transform(ParkingMeters.sf,"EPSG:6339")
 
 ParkingMeters.sf <-
   ParkingMeters.sf%>%
-  dplyr::select(StationID,STREET_BLOCK,POST_ID,Address2,SESSION_START_DT,SESSION_END_DT,GROSS_PAID_AMT,interval60,interval15,week,dotw,ParkTime,
+  dplyr::select(StationID,STREET_BLOCK,POST_ID,SESSION_START_DT,SESSION_END_DT,GROSS_PAID_AMT,interval60,interval15,week,dotw,ParkTime,
                 ParkTimeMin2,GROSS_PAID_AMT2,ParkingRate,Neighborhoods,COLLECTION_SUBROUTE,LONGITUDE2,LATITUDE2)
 # Exploratory
 ParkingMeters.sf.nogeometry <-st_drop_geometry(ParkingMeters.sf)
@@ -250,7 +258,7 @@ ggplot(ParkingMeters.sf.nogeometry %>%
        x="Number of Parking Sessions", 
        y="Number of Blocks")+
   plotTheme()
-
+ParkingMeters.sf.nogeometry%>%
 ggplot(ParkingMeters.sf.nogeometry %>% mutate(hour = hour(SESSION_END_DT)))+
   geom_freqpoly(aes(hour, color = dotw), binwidth = 1)+
   labs(title="Parking Sessions in San Francisco, by day of the week, May, 2018",
@@ -258,22 +266,158 @@ ggplot(ParkingMeters.sf.nogeometry %>% mutate(hour = hour(SESSION_END_DT)))+
        y="Parking Session Count")+
   plotTheme()
 
+#weather
+weather.Panel <- 
+  riem_measures(station = "SFO", date_start = "2018-04-30", date_end = "2018-05-04") %>%
+  dplyr::select(valid, tmpf, p01i, sknt)%>%
+  replace(is.na(.), 0) %>%
+  mutate(interval60 = ymd_h(substr(valid,1,13))) %>%
+  mutate(week = week(interval60),
+         dotw = wday(interval60, label=TRUE)) %>%
+  group_by(interval60) %>%
+  summarize(Temperature = max(tmpf),
+            Precipitation = sum(p01i),
+            Wind_Speed = max(sknt)) %>%
+  mutate(Temperature = ifelse(Temperature == 0, 42, Temperature))
+
+grid.arrange(
+  ggplot(weather.Panel, aes(interval60,Precipitation)) + geom_line() + 
+    labs(title="Percipitation", x="Hour", y="Perecipitation") + plotTheme(),
+  ggplot(weather.Panel, aes(interval60,Wind_Speed)) + geom_line() + 
+    labs(title="Wind Speed", x="Hour", y="Wind Speed") + plotTheme(),
+  ggplot(weather.Panel, aes(interval60,Temperature)) + geom_line() + 
+    labs(title="Temperature", x="Hour", y="Temperature") + plotTheme(),
+  top="Weather Data - San Francisco SFO - March-April, 2018")
+                      
+#join Census
+ParkingMeters.sf <-
+  ParkingMeters.sf%>%
+  st_transform('EPSG:6346')
+sfCensus <-
+  sfCensus %>%
+  st_transform('EPSG:6346')
+ParkingMeter_census <- st_join(ParkingMeters.sf,sfCensus, join = st_within)
+
+#not working at all not sure
+ParkingMeter_censusPLOT <- subset(ParkingMeter_census, dotw %in% c("Mon"))
 ggplot()+
   geom_sf(data = sfTracts %>%
-            st_transform(crs='EPSG:6339'))+
-  geom_point(data = ParkingMeters.sf %>% 
+            st_transform("EPSG:6339"), colour = '#efefef')+
+  geom_point(data = ParkingMeter_censusPLOT %>% 
                mutate(hour = hour(SESSION_END_DT),
                       time_of_day = case_when(hour(interval60) < 7 | hour(interval60) > 18 ~ "Overnight",
                                               hour(interval60) >= 7 & hour(interval60) < 10 ~ "AM Rush",
                                               hour(interval60) >= 10 & hour(interval60) < 15 ~ "Mid-Day",
                                               hour(interval60) >= 15 & hour(interval60) <= 18 ~ "PM Rush"))%>%
-               group_by(StationID,time_of_day) %>%
+               group_by(StationID, LATITUDE2, LONGITUDE2,  time_of_day) %>%
                tally(),
-             aes(x=LONGITUDE2, y = LATITUDE2, color = n), 
-             fill = "transparent", alpha = 0.4, size = 0.3)+
+             aes(x=LONGITUDE2, y = LONGITUDE2, color = n), 
+             fill = "transparent", alpha = 0.4, size = 1.5)+
   scale_colour_viridis(direction = -1,
                        discrete = FALSE, option = "D")+
-  ylim(min(ParkingMeters.sf$LATITUDE2), max(ParkingMeters.sf$LATITUDE2))+
-  xlim(min(ParkingMeters.sf$LONGITUDE2), max(ParkingMeters.sf$LONGITUDE2))+
-  labs(title="Bike share trips per hr by station. Chicago, May, 2018")+
+  ylim(min(ParkingMeter_censusPLOT$LATITUDE2), max(ParkingMeter_censusPLOT$LATITUDE2))+
+  xlim(min(ParkingMeter_censusPLOT$LONGITUDE2), max(ParkingMeter_censusPLOT$LONGITUDE2))+
+  facet_grid(~ time_of_day)+
+  labs(title="Bike share trips per hr by station,\nSF+Alameda, March-April, 2018")+
   mapTheme()
+
+
+
+#study panel stuff
+
+
+study.panel <- 
+  expand.grid(interval60 = unique(ParkingMeters$interval60), 
+              StationID = unique(ParkingMeters$StationID))
+
+ride.panel <- 
+  ParkingMeters.sf %>%
+  mutate(Trip_Counter = 1) %>%
+  right_join(study.panel) %>% 
+  group_by(interval60, StationID) %>%
+  summarize(Trip_Count = sum(Trip_Counter, na.rm=T)) %>%
+  mutate(week = week(interval60),
+         dotw = wday(interval60, label = TRUE))
+ride.panel <- st_drop_geometry(ride.panel)
+
+latlong <- ParkingMeters.sf%>%
+  dplyr::select(StationID, LATITUDE2,LONGITUDE2)
+latlong<- st_drop_geometry(latlong)
+
+latlong <- latlong[!duplicated(latlong$StationID), ]
+ride.panel <- merge(ride.panel, latlong, by='StationID')
+
+
+#join 
+
+ride.panel <- ride.panel[!(ride.panel$LATITUDE2 == ""), ]
+ride.panel<- ride.panel[!(ride.panel$LONGITUDE2 == ""), ]
+ride.panel <- st_as_sf(x = ride.panel, coords = c("LONGITUDE2","LATITUDE2"),crs = "+proj=longlat +crs = 'EPSG:6339'")
+ride.panel<- st_transform(ride.panel,"EPSG:6339")
+
+#lags
+ride.panel <- 
+  ride.panel %>% 
+  arrange(StationID, interval60) %>% 
+  mutate(lagHour = dplyr::lag(Trip_Count,1),
+         lag2Hours = dplyr::lag(Trip_Count,2),
+         lag3Hours = dplyr::lag(Trip_Count,3),
+         lag4Hours = dplyr::lag(Trip_Count,4),
+         lag12Hours = dplyr::lag(Trip_Count,12),
+         lag1day = dplyr::lag(Trip_Count,24))
+
+#split training test
+ride.Train <- subset(ride.panel, dotw %in% c("Mon","Tue","Wed"))
+ride.Test <- subset(ride.panel, dotw %in% c("Thu","Fri"))
+
+#something is wrong unsure
+st_drop_geometry(rbind(
+  mutate(ride.Train, Legend = "Training"), 
+  mutate(ride.Test, Legend = "Testing"))) %>%
+  group_by(Legend, interval60) %>% 
+  summarize(Trip_Count = sum(Trip_Count)) %>%
+  ungroup() %>% 
+  ggplot(aes(interval60, Trip_Count, colour = Legend)) + geom_line() +
+  scale_colour_manual(values = palette2) +
+  labs(title="Parking Sessions by week: May 2018", 
+       x="Day", y="Session Count") +
+  plotTheme() + theme(panel.grid.major = element_blank())    
+
+plotData.lag <-
+  filter(as.data.frame(ride.panel)) %>%
+  dplyr::select(starts_with("lag"), Trip_Count) %>%
+  gather(Variable, Value, -Trip_Count) %>%
+  mutate(Variable = fct_relevel(Variable, "lagHour","lag2Hours","lag3Hours",
+                                "lag4Hours","lag12Hours","lag1day"))
+correlation.lag <-
+  group_by(plotData.lag, Variable) %>%
+  summarize(correlation = round(cor(Value, Trip_Count, use = "complete.obs"), 2)) 
+
+ggplot(plotData.lag, aes(Value, Trip_Count)) +
+  geom_point(size = 0.1) +
+  geom_text(data = correlation.lag, aes(label = paste("r =", round(correlation, 2))),
+            x=-Inf, y=Inf, vjust = 1.5, hjust = -.1) +
+  geom_smooth(method = "lm", se = FALSE, colour = "black") +
+  facet_wrap(~Variable, ncol = 2, scales = "free") +
+  labs(title = "Parking Session Count as Function of time lags") +
+  plotTheme()
+
+#Models
+reg1 <- lm(Trip_Count ~  hour(interval60) + dotw,  data=ride.Train)
+
+reg2 <- lm(Trip_Count ~  StationID + dotw,  data=ride.Train)
+
+reg3 <- lm(Trip_Count ~  StationID + hour(interval60) + dotw , 
+           data=ride.Train)
+
+reg4 <- lm(Trip_Count ~  StationID +  hour(interval60) + dotw +
+             lagHour + lag2Hours + lag3Hours + lag12Hours + lag1day, 
+           data=ride.Train)
+
+
+
+ride.Test.dotwNest <- 
+  ride.Test %>%
+  nest(-dotw) 
+
+ride.Test.weekNest
